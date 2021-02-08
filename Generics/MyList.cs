06fb4e171fe81d9f7e7a877197b152c852c41740
Koray HALAT <korayhalat79@gmail.com>
2021-02-08 15:24:02 +0300

    MyDictionaryOdevDers4

---
+++ Generics/MyList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    class MyList<T>
+    {
+        public void Add(T item)
+        {
+
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sehirler1 = new List<string>();
+            sehirler1.Add("Ankara");
+
+            MyList<string> sehirler2 = new MyList<string>();
+            sehirler2.Add("Ankara");
+        }
+    }
+}
