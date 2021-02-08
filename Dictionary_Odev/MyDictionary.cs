9c99ea025d4917966cc9b9e40289d9d4e6174f37
Koray HALAT <korayhalat79@gmail.com>
2021-02-08 15:37:23 +0300

    4.DersOdev  MyDictionary

---
+++ Dictionary_Odev/MyDictionary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary_Odev
+{
+    class MyDictionary<T>
+    {
+        T[] _array;
+        T[] _tempArray;
+        public MyDictionary()
+        {
+            _array = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            _tempArray = _array;
+            _array = new T[_array.Length + 1];
+
+            for (int i = 0; i < _array.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+            _array[_tempArray.Length - 1] = item;
+
+
+        }
+    }
+}
---
+++ Dictionary_Odev/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary_Odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> motorAdi = new List<string>();
+            motorAdi.Add("Kawasaki");
+            motorAdi.Add("Yamaha");
+            motorAdi.Add("Honda");
+            motorAdi.Add("Bajaj");
+            motorAdi.Add("Suzuki");
+            motorAdi.Add("Mondial");
+
+            MyDictionary<string> myDictionary = new MyDictionary<string>();
+            myDictionary.Add("Kawasaki");
+            myDictionary.Add("Yamaha");
+            myDictionary.Add("Honda");
+            myDictionary.Add("Bajaj");
+            myDictionary.Add("Suzuki");
+
+            Console.WriteLine(motorAdi.Count);
+        }
+    }
+}
